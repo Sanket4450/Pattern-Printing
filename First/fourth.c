27d2a265572f06e04d03adaafead8c826318e829
Sanket4450 <www.talaviyasanket50@gmail.com>
2024-02-15 20:49:16 +0530

    inverted right half pyramid done

---
+++ First/fourth.c
@@ -0,0 +1,19 @@
+// Inverted Right Half Pyramid
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = n; j >= i; j--) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
