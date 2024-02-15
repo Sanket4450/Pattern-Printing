49191d0233360dba449ba3e0b8f83c3f734a18b0
Sanket4450 <www.talaviyasanket50@gmail.com>
2024-02-15 20:50:44 +0530

    inverted left half pyramid done

---
+++ First/fifth.c
@@ -0,0 +1,22 @@
+// Inverted Left Half Pyramid
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j < 2 * i - 1; j++) {
+            printf(" ");
+        }
+        for (int k = n; k >= i; k--) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
